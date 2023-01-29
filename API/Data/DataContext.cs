2d56eda37c395aaa1a31516f62abd90d7b2b0673
wmizera96 <wmizera@outlook.com>
2023-01-29 22:10:21 +0100

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
